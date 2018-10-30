49b8df204ab0de52fc8ec1dafc9a65c1abfcd308
Necroforger <Necroforger@gmail.com>
2018-10-30 06:39:57 -0400

    reduce code size

--- SOLUTIONS/Problem02.hs
+++ SOLUTIONS/Problem02.hs
@@ -20,15 +20,7 @@ import Data.Char
 -- #--------------------------#
 
 main = putStrLn $ show ([(x, isolated x) | x <- ["starwars", "lampung", "secret", "ilmu", "pony"]])
-  where leftHand  = [
-          '1', '2', '3', '4', '5',
-          'q', 'w', 'e', 'r', 't',
-          'a', 's', 'd', 'f', 'g',
-          'z', 'x', 'c', 'v', 'b']
-        rightHand = [x | x <- (['a' .. 'z'] ++ ['0' .. '9']), not (x `elem` leftHand) ]
-        isolated :: String -> Bool
-        isolated w = f w 0
-           where  f []     n = abs n == length w
-                  f (x:xs) n = f xs (n + (if y `elem` leftHand then 1 else if y `elem` rightHand then -1 else 0))
-                    where y = toLower x
-              
+  where isolated :: String -> Bool
+        isolated  x = abs ((sum . (map (\y -> if y `elem` leftHand then 1 else if y `elem` rightHand then -1 else 0))) x) == length x
+          where leftHand    = ['1', '2', '3', '4', '5','q', 'w', 'e', 'r', 't','a', 's', 'd', 'f', 'g','z', 'x', 'c', 'v', 'b']
+                rightHand   = [x | x <- (['a' .. 'z'] ++ ['0' .. '9']), not (x `elem` leftHand) ]
